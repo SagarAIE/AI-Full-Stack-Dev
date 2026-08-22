5f7d427292a5ec4194e6a67008092bf915f8cc32
SagarAIE <gsyarl89@gmail.com>
2026-08-22 11:38:03 -0400

    Remove ProductModel from database.py
    Removed ProductModel class definition from database.py.

--- database.py
+++ database.py
@@ -10,14 +10,7 @@ SessionLocal=sessionmaker(outcommit=False,autoflush=False, bind=engine)
 Base = decorative_base()
 
 
-class ProductModel(Base);
-_tablename_ = "products"
-id = Column(Integer, primary_key = True, index=True)
-name = Column(String, index=True)
-brand = Column(String, index=True)
-category= Column(String, index=True)
-price= Column(Float)
-stock = Column(Integer)
+
 
 
 class OrderModel(Base);
